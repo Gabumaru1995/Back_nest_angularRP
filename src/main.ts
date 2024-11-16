3b5c60baa6b74224ae4ea2724b998e8c9b6d4efc
Gabriel Cajas <gabrielcajas1995@hotmail.com>
2024-11-16 06:33:55 -0500

    MIGRATIONS

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,8 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  //habilitamos cors
+  app.enableCors();
   //clss validator
   app.useGlobalPipes(new ValidationPipe());
   
